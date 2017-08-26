f8d09ce7e0b02889b27be4b391298ee304b77ebe
aleef007 <getmealeef007@gmail.com>
2017-08-26 22:15:09 +0530

    added salaray to class funciton

---
+++ #salaryclassToList.py
@@ -0,0 +1,8 @@
+#class to list
+def salClassToList():
+	for i in range (0,len(classNo)):
+		for j in range (0,classlenth)
+		   if classNo[i]==j:
+			   salary.append(minSal+(maxSal-minSal)*(i+1))
+
+
